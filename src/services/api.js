4d3d1c4ead266500195f1fdcc15daf5e0ea22e17
PedroPaduelo <pedropaduelo@gmail.com>
2021-01-19 00:02:10 +0000

    First

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3010/',
+    baseURL: 'https://3010-ed35dcf4-62da-423c-8abb-828eb9588ea0.ws-us03.gitpod.io/',
 })
 
 export const apiCep = axios.create({
